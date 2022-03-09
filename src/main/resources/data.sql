eaef515b859d5519786e262ebbc97dce377dfe3f
Cassio <cassioate@gmail.com>
2022-03-09 04:05:15 -0300

    Spring-security simples

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$7bVXuoDnYlJbK2dVgNiI.Oyb6JN/hhC2uFK4V6PQiJjBa8qJtkIM6');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
